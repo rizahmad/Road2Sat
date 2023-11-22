055df9b942fa3aeb7fa0d272c48e01430f4c58ba
Rizwan Ahmad <rizwan.bhatti@maanz-ai.com>
2023-11-22 23:37:00 +0500

    updating the scripts to run on new directory structure

--- resources/scripts/video2frames.py
+++ resources/scripts/video2frames.py
@@ -1,16 +1,20 @@
 import cv2
 import os
 import argparse
+import imutils
 
 
-def createFrames(videoPath, destinationPath):
+def createFrames(videoPath, width):
+    destinationPath = '.\\dataset\\frames'
     if not os.path.exists(destinationPath):
         os.makedirs(destinationPath)
     vidcap = cv2.VideoCapture(videoPath)
     success,image = vidcap.read()
     count = 1
     while success:
-        cv2.imwrite(os.path.join(destinationPath, "frame_%d.jpg" % count), image) 
+        resizedImage = imutils.resize(image, width)
+        cv2.imwrite(os.path.join(destinationPath, "frame_%d.jpg" % count), resizedImage) 
+
         success,image = vidcap.read()
         if success:
             print("frame_%d.jpg generated" % count)
@@ -22,7 +26,7 @@ if __name__ == "__main__":
                     description='Creates frames from video',
                     epilog='V1.0')
     parser.add_argument('-v', '--video', help='Full path to video', required=True)
-    parser.add_argument('-d', '--destination', help='Directory to generate frames into', required=True)
+    parser.add_argument('-w', '--width', help='Resize to the width', required=True)
     args = vars(parser.parse_args())
 
-    createFrames(args['video'], args['destination'])
+    createFrames(args['video'], int(args['width']))
